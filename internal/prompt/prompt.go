cc3609dcde5277a790f6cd87d268df61d5c0eece
rafaeldepontes <rafael.cr.carneiro@gmail.com>
2026-02-12 13:09:45 -0300

    feat: adding a prompt.txt for test purpose

---
+++ internal/prompt/prompt.go
@@ -0,0 +1,34 @@
+package prompt
+
+import (
+	"io"
+	"log"
+	"os"
+)
+
+var prompt string
+
+func Get() *string {
+	if prompt == "" {
+		load()
+	}
+	return &prompt
+}
+
+func load() {
+	// For now this is only a test... this should be inside the OS etc...
+	//
+	// And also should be a environment variable instead of hard coded.
+	f, err := os.Open("./internal/prompt/prompt.txt")
+	if err != nil {
+		log.Println("[ERROR] Could not load the prompt...", err)
+		return
+	}
+	defer f.Close()
+	b, err := io.ReadAll(f)
+	if err != nil {
+		log.Println("[ERROR] Could not read the prompt...", err)
+		return
+	}
+	prompt = string(b)
+}
